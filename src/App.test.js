564105661252ee400e5a1c6ab9e023d37225d22b
Vladyslav Shyrokov <pein091@gmail.com>
2022-11-06 13:30:57 -0500

    added formatting rules,applied them to the project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(
+    <App
+      props1={'asd'}
+      props2={'asd'}
+      props3={'asd'}
+      props4={'asd'}
+      props5={'asd'}
+    />
+  )
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
