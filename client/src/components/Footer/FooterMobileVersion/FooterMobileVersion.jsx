3bd6f07ee41269336f378f7bceb56878c66d0a19
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2022-11-17 14:05:32 +0100

    footer constructure redy

--- client/src/components/Footer/FooterMobileVersion/FooterMobileVersion.jsx
+++ client/src/components/Footer/FooterMobileVersion/FooterMobileVersion.jsx
@@ -1,88 +1,86 @@
-import React from 'react'
+import * as React from 'react'
+import Typography from '@mui/material/Typography'
 import {
+  footerMobileVersionStyles,
   Accordion,
-  AccordionDetails,
   AccordionSummary,
-  Typography,
-  Box,
-} from '@mui/material'
-import footerMobileVersionStyles from './Style'
-import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined'
+  AccordionDetails,
+} from './Style'
 
 const FooterMobileVersionComponent = () => {
+  const [expanded, setExpanded] = React.useState('panel1')
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false)
+  }
+
   return (
-    <>
-      <Box sx={footerMobileVersionStyles.Box}>
-        <Accordion>
-          <AccordionSummary
-            aria-controls="panel1d-content"
-            id="panel1d-header"
-            expandIcon={<ChevronRightOutlinedIcon />}
-          >
-            <Typography sx={footerMobileVersionStyles.h4} variant="h4">
-              О компании
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography sx={footerMobileVersionStyles.p} variant="p">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-              pariatur eligendi, laboriosam earum ad asperiores cumque,
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            aria-controls="panel2d-content"
-            id="panel2d-header"
-            expandIcon={<ChevronRightOutlinedIcon />}
-          >
-            <Typography sx={footerMobileVersionStyles.h4} variant="h4">
-              Оплата
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography sx={footerMobileVersionStyles.p} variant="p">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-              pariatur eligendi, laboriosam earum ad asperiores cumque,
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            aria-controls="panel3d-content"
-            id="panel3d-header"
-            expandIcon={<ChevronRightOutlinedIcon />}
-          >
-            <Typography sx={footerMobileVersionStyles.h4} variant="h4">
-              Магазины
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography sx={footerMobileVersionStyles.p} variant="p">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-              pariatur eligendi, laboriosam earum ad asperiores cumque,
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            aria-controls="panel4d-content"
-            id="panel4d-header"
-            expandIcon={<ChevronRightOutlinedIcon />}
-          >
-            <Typography sx={footerMobileVersionStyles.h4} variant="h4">
-              Граффик работы
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography sx={footerMobileVersionStyles.p} variant="p">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-              pariatur eligendi, laboriosam earum ad asperiores cumque,
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </Box>
-    </>
+    <div>
+      <Accordion
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography variant="h6">О нас </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="p" sx={footerMobileVersionStyles.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel2'}
+        onChange={handleChange('panel2')}
+      >
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography variant="h6"> Помощь </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="p" sx={footerMobileVersionStyles.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel3'}
+        onChange={handleChange('panel3')}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography variant="h6">Магазины </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="p" sx={footerMobileVersionStyles.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel4'}
+        onChange={handleChange('panel4')}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography variant="h6">График работы </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="p" sx={footerMobileVersionStyles.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   )
 }
 
--- client/src/components/Footer/FooterMobileVersion/Style.js
+++ client/src/components/Footer/FooterMobileVersion/Style.js
@@ -1,14 +1,11 @@
-const footerMobileVersionStyles = {
-  h4: {
-    fontSize: '1.1rem',
-    fontStyle: 'Regular',
-    textAlign: 'center',
-    fonWeight: '400',
-    textDecoration: 'none',
-    color: '#00FF38',
-    textTransform: 'uppercase',
-    cursorPointer: 'none',
-  },
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp'
+import MuiAccordion from '@mui/material/Accordion'
+import MuiAccordionSummary from '@mui/material/AccordionSummary'
+import MuiAccordionDetails from '@mui/material/AccordionDetails'
+
+export const footerMobileVersionStyles = {
   p: {
     fontSize: '1.1rem',
     fontStyle: 'Regular',
@@ -19,13 +16,44 @@ const footerMobileVersionStyles = {
     '&:hover': {
       color: '#FFFFFF',
     },
-    Box: {
-      width: '100%',
-      maxHeight: '500px',
-      height: '100vh',
-      backgroundColor: '#000000',
-    },
   },
 }
 
-export default footerMobileVersionStyles
+export const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}))
+
+export const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={
+      <ArrowForwardIosSharpIcon sx={{ fontSize: '1.1rem', color: '#FFFFFF' }} />
+    }
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? 'rgba(0, 0, 0)' : 'rgba(0, 0, 0)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(2),
+    color: '#00FF38',
+    fontStyle: 'Regular',
+    textTransform: 'uppercase',
+    cursorPointer: 'none',
+  },
+}))
+
+export const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  backgroundColor: 'rgba(0, 0, 0)',
+}))
