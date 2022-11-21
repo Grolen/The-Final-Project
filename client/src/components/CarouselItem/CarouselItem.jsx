14b966aa6f840dc13e4bc8bfd0e3b8234056a8af
Makar Kovynia <mak.kovynia@gmail.com>
2022-11-21 23:51:26 +0200

    Main Carousel almost done

--- client/src/components/CarouselItem/CarouselItem.jsx
+++ client/src/components/CarouselItem/CarouselItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Paper, Button } from '@mui/material'
+import { Paper } from '@mui/material'
 
 const CarouselItem = (props) => {
   const { item } = props
   return (
     <Paper>
-      <img src={item.image} style={{ width: '100%', height: '100%' }} />
+      <img
+        src={item.image}
+        style={{ width: '100%', height: '704px', objectFit: 'cover' }}
+      />
     </Paper>
   )
 }
--- client/src/components/MainCarousel/MainCarousel.jsx
+++ client/src/components/MainCarousel/MainCarousel.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
 import CarouselItem from '../CarouselItem/CarouselItem'
-import { Container } from '@mui/system'
+import Button from '../Button/Button'
+import { Box, Container } from '@mui/system'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import axios from 'axios'
 
 const MainCarousel = () => {
   var items = [
     {
-      image: '#',
+      image:
+        'https://media.discordapp.net/attachments/1043545360498442271/1043545432682401853/Rectangle_7.jpg',
     },
     {
       image:
-        'https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg',
+        'https://media.discordapp.net/attachments/1043545360498442271/1043545433013760080/Rectangle_76.jpg',
     },
   ]
 
   return (
-    <Container>
+    <Container
+      sx={{
+        position: 'relative',
+      }}
+    >
       <Carousel
         PrevIcon={<ArrowBackIosNewIcon fontSize="large" />}
         NextIcon={<ArrowForwardIosIcon fontSize="large" />}
@@ -46,7 +51,31 @@ const MainCarousel = () => {
           <CarouselItem key={i} item={item} />
         ))}
       </Carousel>
-      <div></div>
+      <Box
+        sx={{
+          position: 'absolute',
+          right: 80,
+          top: 446,
+          width: 500,
+          height: 250,
+          paddingLeft: 2,
+          background: ' rgba(0, 0, 0, 0.25)',
+          zIndex: 1,
+        }}
+      >
+        <h1
+          style={{
+            fontSize: 45,
+            color: '#fff',
+            textShadow: '0px 5px 4px rgba(0, 0, 0, 0.55)',
+            marginTop: 19,
+            marginBottom: 50,
+          }}
+        >
+          НОВАЯ КОЛЛЕКЦИЯ СОВМЕСТНО В VISA
+        </h1>
+        <Button></Button>
+      </Box>
     </Container>
   )
 }
