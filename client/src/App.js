242978522bfd1d24bda337bd4f85602c500e0570
Makar Kovynia <mak.kovynia@gmail.com>
2022-12-07 13:55:50 +0200

    aaaaa

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './styles/App.module.scss'
 import MainCarousel from './components/MainCarousel/MainCarousel'
 import SecondCarousel from './components/SecondCarousel/SecondCarousel'
+import MainZalupa from './components/MainZalupa/MainZalupa'
 
 function App() {
   return (
     <>
-      <SecondCarousel />
+      <MainZalupa />
     </>
   )
 }
---
+++ client/src/components/MainZalupa/MainZalupa.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './MainZalupa.module.scss'
+import { Container, Grid } from '@mui/material'
+
+const MainZalupa = () => {
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <div>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1043545360498442271/1049787225132515399/Rectangle_9.jpg"
+              alt="#"
+            />
+          </div>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1043545360498442271/1049787223412846663/Rectangle_11.jpg"
+              alt="#"
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1043545360498442271/1049787237501513869/Rectangle_10.jpg"
+              alt="#"
+            />
+          </div>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1043545360498442271/1049787206845341758/Rectangle_12.jpg"
+              alt="#"
+            />
+          </div>
+        </div>
+      </Grid>
+    </Container>
+  )
+}
+
+export default MainZalupa
